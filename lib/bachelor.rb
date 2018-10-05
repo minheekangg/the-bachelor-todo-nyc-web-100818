8d6f51a8d7b1c8083316e598a1d5960885dd6366
Min Hee  Park <minheekangg@gmail.com>
2018-10-05 19:32:39 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,13 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |season, keys|
+    if season == season
+    keys.each do |key, value|
+      if key == :status && value == "winner"
+        puts "key"
+      end
+    end
+  end
+end
 end
 
 def get_contestant_name(data, occupation)
