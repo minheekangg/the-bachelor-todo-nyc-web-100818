3678c51be61231d1c877049f57ba7406cf83c079
Min Hee  Park <minheekangg@gmail.com>
2018-10-05 19:57:58 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -16,7 +16,7 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, keys|
 end
 
 def count_contestants_by_hometown(data, hometown)
